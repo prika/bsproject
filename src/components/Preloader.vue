5342903e85975827acb0b46ec89115f38a55424c
SofiaFerreira <prika.altx@gmail.com>
2020-05-26 11:50:52 +0100

    Preloader styles

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -12,47 +12,12 @@
           name: ""
       }
   },
-  
   watch: {
     // call again the method if the route changes
       $route (to, from){
         this.name = to.name
-        console.log(to)
+        //console.log(to)
     }
   }
 }
 </script>
-
-<style lang="scss">
-
-.loader{
-    background: #DEDEDE;
-    position: fixed;
-    z-index: 4;
-    top: 0; bottom: 0; left: 0; right: 0;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    opacity: 1;
-
-    h1{
-      color: #FFF;
-      font-size: 218px;
-    }
-}
-.isLoaded { 
-    //display: none;
-
-    opacity: 0;
-  
-    transition: opacity 1s ease-in .3s;
-    z-index: -3;
-    
-    h1{
-       transition: opacity 1s ease-in .3s;
-    }
-}
-
-
-</style>
