f3da1fb9b43eacba69ad707d62abe6294e287f9e
SofiaFerreira <prika.altx@gmail.com>
2020-06-08 20:32:01 +0100

    Adding Carousel Bootstrap

--- src/components/BannerFullsize.vue
+++ src/components/BannerFullsize.vue
@@ -6,27 +6,38 @@
         <div class="line verticalLeftLine"></div>
         <div class="line verticalRightLine"></div>
 
-         <!--v-carousel>
-            <v-carousel-item
-                v-for="(item,i) in fotos"
-                :key="i"
-                :src="item.src"></v-carousel-item>
-        </v-carousel-->
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="../assets/images/banners/BlocoB-7234_Banner.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="../assets/images/banners/banner_stone_foto_1.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="../assets/images/banners/banner_stone_foto_2.jpg" alt="Third slide">
+                </div>
+            </div>
+
+            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
     </section>
 </template>
 
 
 <script>
-// export default {
-// 	data() {
-// 		return {
-// 			fotos: [
-// 				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-// 				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-// 				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-// 				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-// 			]
-// 		}
-// 	}
-// }
+
+export default {
+    beforeCreate() {
+        document.getElementsByClassName('carousel').carousel();
+    }
+}
 </script>
