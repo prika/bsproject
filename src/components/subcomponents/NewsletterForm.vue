c5f3d29dd291bdf7e3cf2b4f92e99103e8b2dd03
SofiaFerreira <prika.altx@gmail.com>
2020-07-06 21:33:19 +0100

    Update newsletter request errors and translations

--- src/components/subcomponents/NewsletterForm.vue
+++ src/components/subcomponents/NewsletterForm.vue
@@ -7,6 +7,7 @@
         @submit.prevent="checkForm"
         novalidate="true">
 
+        <transition enter-active-class="animated fadeIn faster" leave-active-class="animated fadeOut faster">
         <div v-if="!success">
             <div class="input_group col-9">  
                 <input id="emailnewsletter"
@@ -14,23 +15,24 @@
                         type="email"
                         name="email"
                         :aria-label="$t('footer-input-email')" placeholder=" ">
-                <label for="form1">Email</label>
+                <label for="form1">{{ $t('footer-input-email') }}</label>
                 <span class="bar"></span>
             </div>
 
             <input class="button" type="submit" :aria-label="$t('footer-submit-email')">
             
             <p style="margin-top: 36px; color: red">
-                <span v-if="errors.length">{{ $t('footer-newsletter-error') }}</span>
-                <ul v-if="errors.length">
-                    <li v-for="error in errors">{{ error }}</li>
-                </ul>
+                <!--span v-if="errors">{{ $t('footer-newsletter-error') }}</!span-->
+                <span>{{ errors }}</span>
             </p>
         </div>
+        </transition>
 
+        <transition enter-active-class="animated fadeIn faster" leave-active-class="animated fadeOut faster">
         <div class="NewsletterRequestSubmited" v-if="success">
-            <p>O seu endereço de email foi adicionado com sucesso à nossa newsletter.</p>
+            <p>{{ $t('footer-newsletter-success') }}</p>
         </div>
+        </transition>
     </form>
 </div>
 </template>
@@ -40,7 +42,7 @@
 export default {
     data() {
         return {
-            errors: [],
+            errors: '',
             email: null,
             success: false
         }
@@ -59,10 +61,7 @@ export default {
                 this.success = true
                 
                 setTimeout(function(){
-                    
-                    debugger
                     self.success = false
-
                 }, 2500)
 
             }).catch((e) => {
@@ -90,3 +89,16 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+
+#newsletter{
+    position: relative;
+    min-height: 140px;
+
+     .NewsletterRequestSubmited{
+        position: absolute; 
+        top: 0; left: 0;
+    } 
+}
+</style>
