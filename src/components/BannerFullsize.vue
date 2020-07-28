be7009bbeace29ad18da092c5f90757e06952bc9
SofiaFerreira <prika.altx@gmail.com>
2020-07-28 16:54:11 +0100

    Implementing SliderGallery

--- src/components/BannerFullsize.vue
+++ src/components/BannerFullsize.vue
@@ -1,43 +1,51 @@
 <template>
     <section id="bannerFullsize">
+        <div class="line horizontalLine"></div>
+        <div class="line verticalLeftLine"></div>
+        <div class="line verticalRightLine"></div>
 
-        <div class="carouselContainer">
-
-            <ol class="carousel-indicators">
-                <li     v-for="(image, index) in imageGroupSliderGallery"  
-                        :class="(index === 0 ? 'active': '')" 
-                        data-target="#carouselExampleFade" 
-                        :data-slide-to="index">
+            <div class="carousel-indicators">
+                <a  v-for="(image, index) in imageGroupSliderGallery"  
+                    :class="(index === activeSlide ? 'active': '')"
+                    :data-slide-to="index"
+                    href="javascript:void(0)"
+                    @click="selectSlide(index)">
                         
                         <span v-if="index < 9">0</span>{{index+1}}
-                </li>
-            </ol>
+                </a>
+            </div>
 
+            <!--button class="prev" @click="prev">&#10094; Previous</button -->
+            <!--button class="next" @click="next">&#10095; Next</button -->
 
+      
+            <div class="carouselContainer">
             <template v-for="(image, index) in imageGroupSliderGallery">
                 <!-- Image -->
-                <div v-if="image.type == 'img'" :class="['slide', (index === 0 ? 'active': '')]"
+                <div v-if="image.type == 'img'" 
+                    :class="['slide', (index === activeSlide ? 'active': '')]"
                     :width="newResizedWidth + 'px'" 
                     :height="newResizedHeight + 'px'"
                     :key="image.id">
+
                     <img    :src="getImgUrl(image.src)" 
                             :alt="image.alt"
-                            v-model="slide"
                             :width="newResizedWidth + 'px'" 
                             :height="newResizedHeight + 'px'"
                             :importance="(index === 0 ? 'high': 'low')">
                 </div>
 
-                 <!-- Video Mp4 -->
-                <vue-plyr v-if="image.type == 'video-uploaded'" :class="['slide', (index === 0 ? 'active': '')]" 
+                 <!-- Video HTML5 -->
+                <vue-plyr   v-if="image.type == 'video-uploaded'" 
+                            :class="['slide', (index === activeSlide ? 'active': '')]" 
                             :ref="'player'+index"
                             :width="newResizedWidth + 'px'" 
                             :height="newResizedHeight + 'px'"
                             :key="image.id">
-                    <video  loop poster playsinline autoplay
+
+                    <video  loop poster
                             :poster="getImgUrl(image.src)" 
                             :src="getVideoUrl(image.srcvideo)"
-                            v-model="slide"
                             :aria-label="image.alt"
                             :width="newResizedWidth + 'px'" 
                             :height="newResizedHeight + 'px'"
@@ -47,23 +55,40 @@
                     </video>
                 </vue-plyr>
 
-                <!-- Video Vimeo -->
-                <vue-plyr v-if="image.type == 'video-vimeo'" :class="['slide', (index === 0 ? 'active': '')]" :ref="'player'+index" 
-                        :width="newResizedWidth + 'px'" 
-                        :height="newResizedHeight + 'px'"
-                        :key="image.id">
-
-                    <div    class="plyr__video-embed" 
+                <!-- Video Youtube -->
+                <vue-plyr   v-if="image.type == 'video-youtube'" 
+                            :class="['slide', (index === activeSlide ? 'active': '')]" 
+                            :ref="'player'+index" 
                             :width="newResizedWidth + 'px'" 
-                            :height="newResizedHeight + 'px'" >
+                            :height="newResizedHeight + 'px'"
+                            :key="image.id">
+                    <div class="plyr__video-embed">
+                        <iframe     :width="newResizedWidth + 'px'" 
+                                    :height="newResizedHeight + 'px'"
+                                    :alt="image.alt"
+                                    :poster="image.src"
+                                    :src="image.srcvideo"
+                                    :importance="(index === 0 ? 'high': 'low')"
+                                    allowtransparency>
+                        </iframe>
+                    </div>
+                </vue-plyr>
 
-                        <iframe v-model="slide"
-                                :alt="image.alt"
+                <!-- Video Vimeo -->
+                <vue-plyr   v-if="image.type == 'video-vimeo'" 
+                            :class="['slide', (index === activeSlide ? 'active': '')]" 
+                            :ref="'player'+index"
+                            :width="newResizedWidth + 'px'" 
+                            :height="newResizedHeight + 'px'"
+                            :key="image.id">
+
+                    <div    class="plyr__video-embed">
+
+                        <iframe :alt="image.alt"
                                 :width="newResizedWidth + 'px'" 
                                 :height="newResizedHeight + 'px'"
                                 :poster="getImgUrl(image.src)"
                                 :src="image.srcvideo"
-                                :style="{ width: newResizedWidth, height: newResizedHeight}"
                                 :importance="(index === 0 ? 'high': 'low')"
                                 allow="autoplay"
                                 allowfullscreen allowtransparency>
@@ -71,13 +96,19 @@
                     </div>
                 </vue-plyr>
             </template>
+            </div>
 
-        </div>
+       
     </section>
 </template>
 
 <script>
+import VuePlyr from 'vue-plyr'
 export default {
+    name: "bannerFullsize",
+    components: {
+        VuePlyr
+    },
     data() {
         return {
             imageGroupSliderGallery:[
@@ -89,6 +120,8 @@ export default {
                     height: Number
                 }
             ],
+            slidesGroup:[],
+            activeSlide: 0,
             slide: '',
             slideWidth: 0,
             slideHeight: 0,
@@ -99,7 +132,10 @@ export default {
             resizedWidth: 0,
             resizedHeight: 0,
             newResizedWidth: '',
-            newResizedHeight: ''
+            newResizedHeight: '',
+            timer: null,
+            controls: true,
+            pause: false
         }
     },
     created() {
@@ -107,17 +143,22 @@ export default {
         this.$http.get('../mocks/homepage-mock.json').then(response => {
 
             this.imageGroupSliderGallery = response.data.slidergallery
-            if ( this.imageGroupSliderGallery[0].type !== "img" ) {
-                let plyr__video = document.createElement("script")
-                plyr__video.setAttribute("type", "text/javascript")
-                plyr__video.setAttribute("src", "https://cdn.plyr.io/3.6.2/plyr.polyfilled.js")
-                document.head.appendChild(plyr__video)
+
+            for (let i = 0; i < this.imageGroupSliderGallery.length; i++){
+
+                if ( this.imageGroupSliderGallery[i].type !== "img" ) {
+                    let plyr__video = document.createElement("script")
+                    plyr__video.setAttribute("type", "text/javascript")
+                    plyr__video.setAttribute("src", "https://cdn.plyr.io/3.6.2/plyr.polyfilled.js")
+                    document.head.appendChild(plyr__video)
+                }
+
+                this.slideWidth = this.imageGroupSliderGallery[i].width
+                this.slideHeight = this.imageGroupSliderGallery[i].height
+                this.slideAspectRatio = this.slideWidth / this.slideHeight
+                this.resizeContent()
             }
 
-            this.slideWidth = this.imageGroupSliderGallery[0].width
-            this.slideHeight = this.imageGroupSliderGallery[0].height
-            this.slideAspectRatio = this.slideWidth / this.slideHeight
-            this.resizeContent()
             window.addEventListener('resize', this.resizeContent )
             
         })
@@ -126,6 +167,9 @@ export default {
     // destroyed() {
     //     window.removeEventListener('resize', this.resizeContent);
     // },
+    mounted: function() {
+        this.startSlide();
+    },
     methods: {
         resizeContent() {
             
@@ -149,6 +193,33 @@ export default {
         },
         getVideoUrl: function (src) {
             return require( '@/assets/media/'+src )
+        },
+        startSlide: function() {
+            //this.timer = setInterval(this.next, 10000);
+        },
+        selectSlide: function (slideNumber){
+            //this.timer = setInterval(this.next, 20000);
+            this.activeSlide = slideNumber
+        },
+        next: function() {
+            this.activeSlide += 1;
+
+            if ( this.activeSlide == this.imageGroupSliderGallery.length ){
+                this.activeSlide = 0
+            }
+        },
+        prev: function() {
+            this.activeSlide -= 1;
+
+            if ( this.activeSlide == 0 ){
+                this.activeSlide = this.imageGroupSliderGallery.length
+            }
+        }
+
+    },
+    computed: {
+        currentSlide: function() {
+            return this.images[Math.abs(this.activeSlide) % this.imageGroupSliderGallery.length];
         }
     },
     watch: {
@@ -158,19 +229,4 @@ export default {
         // }
     }
 }
-
-
-
-//         mode: 'fade',
-//         auto: true,
-//         speed: 1000,
-//         pause: 6000,
-//         responsive: false,
-//         adaptiveHeight: false,
-//         controls: false,
-//         onSlideBefore: function() {
-//           slider.stopAuto();
-//           slider.startAuto();
-//         }
-//       }); 
 </script>
