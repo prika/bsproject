e1a62fb8ceca5b7f50b6eb41fd68110261d01af5
SofiaFerreira <prika.altx@gmail.com>
2020-07-30 12:46:40 +0100

    Minor refactoring and cleanup.

--- src/components/BannerFullsize.vue
+++ src/components/BannerFullsize.vue
@@ -13,80 +13,88 @@
                         
                         <span v-if="index < 9">0</span>{{index+1}}
                 </a>
+
+                 <a @click="pauseMovie()">Pause movie</a>
             </div>
 
             <!--button class="prev" @click="prev">&#10094; Previous</button -->
             <!--button class="next" @click="next">&#10095; Next</button -->
 
-      
             <div class="carouselContainer">
             <template v-for="(image, index) in imageGroupSliderGallery">
                 <!-- Image -->
                 <div v-if="image.type == 'img'" 
                     :class="['slide', (index === activeSlide ? 'active': '')]"
-                    :width="newResizedWidth + 'px'" 
-                    :height="newResizedHeight + 'px'"
+                    :width="resizedWidth[index] + 'px'" 
+                    :height="resizedHeight[index] + 'px'"
                     :key="image.id">
 
                     <img    :src="getImgUrl(image.src)" 
                             :alt="image.alt"
-                            :width="newResizedWidth + 'px'" 
-                            :height="newResizedHeight + 'px'"
+                            :width="resizedWidth[index] + 'px'" 
+                            :height="resizedHeight[index] + 'px'"
                             :importance="(index === 0 ? 'high': 'low')">
                 </div>
 
                  <!-- Video HTML5 -->
-                <vue-plyr   v-if="image.type == 'video-uploaded'" 
+                <div   v-if="image.type == 'video-uploaded'" 
                             :class="['slide', (index === activeSlide ? 'active': '')]" 
                             :ref="'player'+index"
-                            :width="newResizedWidth + 'px'" 
-                            :height="newResizedHeight + 'px'"
+                            :width="resizedWidth[index] + 'px'" 
+                            :height="resizedHeight[index] + 'px'"
                             :key="image.id">
 
-                    <video  loop poster
+                    <video  :id="'video'+index"
+                            loop poster crossorigin playsinline
                             :poster="getImgUrl(image.src)" 
                             :src="getVideoUrl(image.srcvideo)"
                             :aria-label="image.alt"
-                            :width="newResizedWidth + 'px'" 
-                            :height="newResizedHeight + 'px'"
+                            :width="resizedWidth[index] + 'px'" 
+                            :height="resizedHeight[index] + 'px'"
                             :importance="(index === 0 ? 'high': 'low')">
                         <source :src="getVideoUrl(image.srcvideo)" type="video/mp4" size="720">
                         <source :src="getVideoUrl(image.srcvideo1080)" type="video/mp4" size="1080">
                     </video>
-                </vue-plyr>
+                </div>
 
                 <!-- Video Youtube -->
-                <vue-plyr   v-if="image.type == 'video-youtube'" 
+                <div   v-if="image.type == 'video-youtube'" 
                             :class="['slide', (index === activeSlide ? 'active': '')]" 
                             :ref="'player'+index" 
-                            :width="newResizedWidth + 'px'" 
-                            :height="newResizedHeight + 'px'"
-                            :key="image.id">
-                    <div class="plyr__video-embed">
-                        <iframe     :width="newResizedWidth + 'px'" 
-                                    :height="newResizedHeight + 'px'"
+                            :width="resizedWidth[index] + 'px'" 
+                            :height="resizedHeight[index] + 'px'"
+                            :key="image.id"
+                            class="plyr__video-embed"
+                            data-plyr-provider="youtube"
+                            :id="'video'+index">
+               
+                        <iframe     
+                                    :width="resizedWidth[index] + 'px'" 
+                                    :height="resizedHeight[index] + 'px'"
                                     :alt="image.alt"
                                     :poster="image.src"
                                     :src="image.srcvideo"
                                     :importance="(index === 0 ? 'high': 'low')"
-                                    allowtransparency>
+                                    allowtransparency  allow="autoplay">
                         </iframe>
-                    </div>
-                </vue-plyr>
+                   
+                </div>
 
                 <!-- Video Vimeo -->
                 <vue-plyr   v-if="image.type == 'video-vimeo'" 
                             :class="['slide', (index === activeSlide ? 'active': '')]" 
                             :ref="'player'+index"
-                            :width="newResizedWidth + 'px'" 
-                            :height="newResizedHeight + 'px'"
+                            :width="resizedWidth[index] + 'px'" 
+                            :height="resizedHeight[index] + 'px'"
+                            data-plyr-provider="vimeo"
                             :key="image.id">
 
-                    <div    class="plyr__video-embed">
+                    <div    class="plyr__video-embed" :id="'video'+index">
 
-                        <iframe :alt="image.alt"
-                                :width="newResizedWidth + 'px'" 
-                                :height="newResizedHeight + 'px'"
+                        <iframe 
+                                :alt="image.alt"
+                                :width="resizedWidth[index] + 'px'" 
+                                :height="resizedHeight[index] + 'px'"
                                 :poster="getImgUrl(image.src)"
                                 :src="image.srcvideo"
                                 :importance="(index === 0 ? 'high': 'low')"
@@ -120,22 +128,20 @@ export default {
                     height: Number
                 }
             ],
-            slidesGroup:[],
             activeSlide: 0,
             slide: '',
-            slideWidth: 0,
-            slideHeight: 0,
-            slideAspectRatio: 0,
+            slideWidth: [],
+            slideHeight: [],
+            slideAspectRatio: [],
             windowHeight: 0,
             windowWidth: 0,
             windowRatio: 0,
-            resizedWidth: 0,
-            resizedHeight: 0,
-            newResizedWidth: '',
-            newResizedHeight: '',
+            resizedWidth: [],
+            resizedHeight: [],
             timer: null,
             controls: true,
-            pause: false
+            pause: false,
+            player: null
         }
     },
     created() {
@@ -144,49 +150,48 @@ export default {
 
             this.imageGroupSliderGallery = response.data.slidergallery
 
-            for (let i = 0; i < this.imageGroupSliderGallery.length; i++){
+            for (let i = 0; i < this.imageGroupSliderGallery.length; i++) {
 
-                if ( this.imageGroupSliderGallery[i].type !== "img" ) {
-                    let plyr__video = document.createElement("script")
-                    plyr__video.setAttribute("type", "text/javascript")
-                    plyr__video.setAttribute("src", "https://cdn.plyr.io/3.6.2/plyr.polyfilled.js")
-                    document.head.appendChild(plyr__video)
-                }
-
-                this.slideWidth = this.imageGroupSliderGallery[i].width
-                this.slideHeight = this.imageGroupSliderGallery[i].height
-                this.slideAspectRatio = this.slideWidth / this.slideHeight
-                this.resizeContent()
+                if (this.imageGroupSliderGallery[i].type === "img") continue
+                this.appendPlayerJS()
+                break
             }
 
+            this.resizeContent()
             window.addEventListener('resize', this.resizeContent )
-            
+            this.startSlide();
         })
-
     },
-    // destroyed() {
-    //     window.removeEventListener('resize', this.resizeContent);
-    // },
-    mounted: function() {
-        this.startSlide();
+    destroyed() {
+        window.removeEventListener('resize', this.resizeContent);
     },
     methods: {
-        resizeContent() {
-            
+        appendPlayerJS: function() {
+            let plyr__video = document.createElement("script")
+            plyr__video.setAttribute("type", "text/javascript")
+            plyr__video.setAttribute("src", "https://cdn.plyr.io/3.6.2/plyr.polyfilled.js")
+            document.head.appendChild(plyr__video)
+        },
+        resizeContent: function() {
+
             this.windowWidth = window.innerWidth
             this.windowHeight = window.innerHeight
             this.windowRatio = this.windowWidth / this.windowHeight
 
-            if ( this.slideAspectRatio < this.windowRatio ){
-                this.resizedWidth = this.windowWidth
-                this.resizedHeight =  this.resizedWidth / this.slideAspectRatio
-            } else {
-                this.resizedHeight = this.windowHeight
-                this.resizedWidth = this.resizedHeight * this.slideAspectRatio
-            }
+            for (let i = 0; i < this.imageGroupSliderGallery.length; i++) {
 
-            this.newResizedWidth = this.resizedWidth 
-            this.newResizedHeight = this.resizedHeight 
+                this.slideWidth[i] = this.imageGroupSliderGallery[i].width
+                this.slideHeight[i] = this.imageGroupSliderGallery[i].height
+                this.slideAspectRatio[i] = this.slideWidth[i] / this.slideHeight[i]
+                
+                if (this.slideAspectRatio[i] < this.windowRatio) {
+                    this.resizedWidth[i] = this.windowWidth
+                    this.resizedHeight[i] =  this.resizedWidth[i] / this.slideAspectRatio[i]
+                } else {
+                    this.resizedHeight[i] = this.windowHeight
+                    this.resizedWidth[i] = this.resizedHeight[i] * this.slideAspectRatio[i]
+                }
+            }
         },
         getImgUrl: function (src) {
             return require( '@/assets/images/'+src )
@@ -195,38 +200,39 @@ export default {
             return require( '@/assets/media/'+src )
         },
         startSlide: function() {
-            //this.timer = setInterval(this.next, 10000);
+            
+            //this.timer = setInterval(this.next, 5000) 
         },
-        selectSlide: function (slideNumber){
-            //this.timer = setInterval(this.next, 20000);
+        selectSlide: function (slideNumber) {            
+            this.pauseMovie(this.activeSlide)
             this.activeSlide = slideNumber
+            this.playMovie(this.activeSlide) 
         },
         next: function() {
-            this.activeSlide += 1;
-
-            if ( this.activeSlide == this.imageGroupSliderGallery.length ){
-                this.activeSlide = 0
-            }
+            this.pauseMovie(this.activeSlide)
+            this.activeSlide = this.activeSlide + 1 ==  this.imageGroupSliderGallery.length ?  0 : this.activeSlide + 1
+            this.playMovie(this.activeSlide) 
         },
         prev: function() {
-            this.activeSlide -= 1;
+            this.pauseMovie(this.activeSlide)
+            this.activeSlide = (this.activeSlide - 1 == 0) ? this.imageGroupSliderGallery.length : this.activeSlide - 1
+            this.playMovie(this.activeSlide) 
+        },
+        playMovie: function(slideNumber) {
 
-            if ( this.activeSlide == 0 ){
-                this.activeSlide = this.imageGroupSliderGallery.length
-            }
+            if (this.imageGroupSliderGallery[slideNumber].type == "img") return
+            debugger
+            var playElement = document.getElementById("video" + slideNumber) 
+            playElement.play()
+        },
+        pauseMovie: function(slideNumber) {
+
+            if (this.imageGroupSliderGallery[slideNumber].type == "img") return
+            var pauseElement = document.getElementById("video" +  slideNumber)
+            debugger
+            pauseElement.pause()
         }
 
-    },
-    computed: {
-        currentSlide: function() {
-            return this.images[Math.abs(this.activeSlide) % this.imageGroupSliderGallery.length];
-        }
-    },
-    watch: {
-        // windowHeight(newHeight, oldHeight) {
-        //     console.log(oldHeight)
-        //     console.log(newHeight)
-        // }
     }
 }
 </script>
