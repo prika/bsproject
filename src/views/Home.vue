9085fd86955f30a114fbbb4a9d7a59bec0b168ab
SofiaFerreira <prika.altx@gmail.com>
2020-08-04 15:48:30 +0100

    Adding read more button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,8 @@
           <div class="pageContentText col-12 col-md-5 order-2 order-sm-2">
               <h1 v-html="home.title">{{home.title}}</h1>
               <p v-html="home.content">{{home.content}}</p>
+              
+              <a href="" class="basicLink"><arrowRightIcon />{{ $t('readlink') }}</a>
           </div>
       </ArticleParallaxSmall>
 
@@ -14,17 +16,16 @@
           {{home.featured_text1}}<span>{{home.featured_text2}}</span>
       </ProductsList>
 
-
       <NewsSlider>
-        <h1 class="pageTitleh2 h2">notícias</h1>
+          <h1 class="pageTitleh2 h2">{{ $t('home-title-page') }}</h1>
       </NewsSlider>
   </div>
 </template>
 
-
 <script>
 import BannerFullsize from '../components/BannerFullsize'
 import ArticleParallaxSmall from '@/components/ArticleParallaxSmall'
+import arrowRightIcon from '@/components/ui/arrow-slim-right.vue'
 import ProductsList from '@/components/ProductsList'
 import NewsSlider from '@/components/NewsSlider'
 
@@ -33,6 +34,7 @@ export default {
   components: {
       BannerFullsize,
       ArticleParallaxSmall,
+      arrowRightIcon,
       ProductsList,
       NewsSlider
   },
@@ -79,3 +81,42 @@ export default {
     }
 }
 </script>
+<style lang="scss">
+.basicLink{
+    font-family: 'Oswald', sans-serif;
+    font-size: 15px;
+    font-weight: 300;
+    letter-spacing: 2px;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: #000;
+    position: relative;
+    margin-top: 5%;
+
+    &::after{
+      position: absolute;
+      top: 11px;
+      right: -60px;
+      content: '';
+      display: block;
+      height: 1px;
+      width: 40px;
+      background-color: #000;
+    }
+
+    .arrowSlimIcon{
+      position: absolute;
+      top: -3px;
+      right: -60px;
+
+      svg line, svg path { stroke: #000; }
+    }
+
+    &:hover{
+        color: #000;
+
+        &::after{background-color: #B76646;}
+        svg line, svg path { stroke: #B76646 }
+    }
+}
+</style>
