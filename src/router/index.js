0ff3f6bd194a2011d7331fa8a3548ad1eb1a341d
Remove landscape orientation fixer <>
2020-08-17 14:17:49 +0100

    Removing not used functions in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-//import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
@@ -97,15 +96,12 @@ const routes = [
   {
     path: '/account',
     name: 'account',
-    component: () => import( /* webpackChunkName: "account-group" */ '../views/AccountInfo.vue'),
-    meta: { requiresAuth: true } // a meta field
-
+    component: () => import( /* webpackChunkName: "account-group" */ '../views/AccountInfo.vue')
   },
   {
     path: '/shoppingcart',
     name: 'shoppingcart',
-    component: () => import( /* webpackChunkName: "shopping-group" */ '../views/ShoppingCart.vue'),
-    meta: { requiresAuth: true } // a meta field
+    component: () => import( /* webpackChunkName: "shopping-group" */ '../views/ShoppingCart.vue')
   }
 ]
 
@@ -116,23 +112,6 @@ const router = new VueRouter({
     scrollBehavior (to, from, savedPosition) {
         return { x: 0, y: 0 }
     }
-})
-
-router.beforeResolve((to, from, next) => {
-  if ( to.name ) {
-      //this.name = to.name
-      console.log( "router name - "+to.name )
-  }
-  next()
-})
-
-// router.beforeEach((to, from, next) => {
-//   if (to.name !== 'shoppingcart' && !isAuthenticated) next({ name: 'Login' })
-//   else next()
-// })
-
-router.afterEach((to, from) => {
-    console.log( "router - after link" )
 })
 
 export default router
