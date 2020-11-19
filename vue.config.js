c85dbb1503e6676d0e45bc184ca683c5e5c2e84c
SofiaFerreira <prika.altx@gmail.com>
2020-11-19 13:43:59 +0000

    Change Vue.config to accept pages

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  chainWebpack: config => {
+  /* chainWebpack: config => {
+    config.plugins.delete('prefetch'),
+    config.plugin('preload').tap((options) => {
+      options[0].include = 'allChunks'
+      return options
+    }),
     config.module
       .rule('images')
         .use('url-loader')
           .loader('url-loader')
           .tap(options => Object.assign(options, { limit: 10240 }))
-  },
+  }, */
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -14,7 +19,28 @@ module.exports = {
       enableInSFC: true
     }
   },
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    subpage: 'src/main.js'
+  },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/bstone/'
+    ? '/'
     : '/'
 }
